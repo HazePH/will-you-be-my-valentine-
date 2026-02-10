512f926e3c0530031f9aa9f51a988c3133f94d21
HazePH <dreimanulat@gmail.com>
2026-02-10 18:34:24 +0800

    Refactor script.js to update response message

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("click", () => {
 yesBtn.addEventListener("click", () => {
   response.innerHTML = `
     <h2>😊 You said YES! 💖</h2>
-    <img src="https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif">
+    <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjE2czJidXhqcTN3ZWNqaWhuczdoZXcyZGFnZGQ4ajRlbTd2YnQ0NSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/iWgIt7lFCsKuVOhq8z/giphy.gif">
     <p>You just made my day 🌸</p>
   `;
   confetti({
@@ -40,3 +40,4 @@ function moveNoButton() {
   noBtn.style.left = `${randomX}px`;
   noBtn.style.top = `${randomY}px`;
 }
+
